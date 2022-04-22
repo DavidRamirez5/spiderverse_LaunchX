3431b3ae8f0038ce7fdd4386cff72890dcdd62de
David Patiño <david.andrespr5@gmail.com>
2022-04-22 16:20:30 -0500

    Agregando test 1

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,8 +1,13 @@
 
-
+const Spiderman = require("./../app/spiderman")
 describe("Test Suite Dummy Description", ()=>{
-    test('Case 1 Dummy',() =>{
-        const resultOfSomething = 1+2
-        expect(resultOfSomething).toBe(18)
+    test('1) Create an spiderman object',() =>{
+        // Paso uno para TDD.
+        const tomHolland= new Spiderman("Spiderman Sony",31,"Andrew Garfield",3,'Sony')
+        expect(tomHolland.name).toBe('Spiderman Sony')
+        expect(tomHolland.age).toBe(31)
+        expect(tomHolland.actor).toBe('Andrew Garfield')
+        expect(tomHolland.movies).toBe(3)
+        expect(tomHolland.studio).toBe('Sony')
     })
 })
